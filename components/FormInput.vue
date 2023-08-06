8d4ec83f59f8db484e15e893ae72a01308d126aa
Bruno Braga <brunobragaw8t@gmail.com>
2023-08-06 15:20:57 +0100

    feature: create FormInput component

---
+++ components/FormInput.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  type: 'text' | 'password' | 'email'
+  placeholder?: string
+  label?: string
+  icon?: string
+  required?: boolean
+  focus?: boolean
+}>(), {
+  placeholder: '',
+  label: '',
+  icon: '',
+  required: false,
+  focus: false
+})
+
+const value = defineModel()
+</script>
+
+<template>
+  <div class="input-group">
+    <span v-if="icon" class="input-group-text">
+      <i :class="icon" />
+    </span>
+
+    <div v-if="label" class="form-floating">
+      <input
+        :id="$.uid.toString()"
+        v-model="value"
+        v-focus="focus"
+        :type="type"
+        :placeholder="placeholder"
+        :required="required"
+        class="form-control"
+      >
+
+      <label :for="$.uid.toString()">{{ label }}</label>
+    </div>
+
+    <input
+      v-else
+      v-model="value"
+      v-focus="focus"
+      :type="type"
+      :placeholder="placeholder"
+      :required="required"
+      class="form-control"
+    >
+  </div>
+</template>
