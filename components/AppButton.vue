1995f104134151a03eb9c5b069d7f5764321cd58
Bruno Braga <brunobragaw8t@gmail.com>
2023-08-06 15:05:35 +0100

    feature: create AppButton component

---
+++ components/AppButton.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  tag: string
+  label: string
+  icon?: string
+  iconPosition?: 'left' | 'right'
+  size?: 'sm' | 'md' | 'lg'
+  variant?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
+  loading?: boolean
+}>(), {
+  icon: '',
+  iconPosition: 'left',
+  size: 'md',
+  variant: 'primary'
+})
+</script>
+
+<template>
+  <component
+    :is="tag"
+    :class="`btn btn-${variant} btn-${size}`"
+    :disabled="loading"
+  >
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+
+    <i v-if="icon && 'left' === iconPosition" :class="`${icon} me-1`" />
+
+    {{ label }}
+
+    <i v-if="icon && 'right' === iconPosition" :class="`${icon} ms-1`" />
+  </component>
+</template>
+
+<style scoped>
+.spinner-border {
+  width: 1rem;
+  height: 1rem;
+  margin-right: 0.5rem;
+}
+</style>
