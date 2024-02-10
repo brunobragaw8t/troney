334686f3b6b320b2d7f8c961f4d761cab5d38aba
Bruno Braga <brunobragaw8t@gmail.com>
2024-02-10 15:13:09 +0000

    chore: disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: {
+    enabled: false
+  },
   modules: ['@nuxtjs/supabase', 'nuxt-icon'],
   supabase: {
     redirectOptions: {
