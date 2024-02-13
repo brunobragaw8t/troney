d55df361821832de49372d4959429a281330bba4
Bruno Braga <brunobragaw8t@gmail.com>
2024-02-13 18:53:09 +0000

    feature: get month from query param and default to current one

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,40 @@
+<script setup lang="ts">
+const route = useRoute()
+
+let year: number
+let month: number
+
+if (
+  typeof route.query.month === 'string' &&
+  /^\d{4}-\d{2}$/.test(route.query.month)
+) {
+  const [yearStr, monthStr] = route.query.month.split('-')
+  year = parseInt(yearStr)
+  month = parseInt(monthStr)
+} else {
+  year = new Date().getFullYear()
+  month = new Date().getMonth() + 1
+}
+
+const daysInMonth = new Date(year, month, 0).getDate()
+const monthName = new Date(year, month - 1).toLocaleString('en-GB', { month: 'long' })
+</script>
+
 <template>
-  <h1>Index</h1>
+  <div>
+    <div class="d-flex align-items-center justify-content-between">
+      <h2>{{ monthName }} {{ year }}</h2>
+
+      <div class="d-flex gap-1">
+        <AppButton label="Previous" size="sm" variant="secondary" />
+        <AppButton label="Next" size="sm" variant="secondary" />
+      </div>
+    </div>
+
+    <p>{{ route.query }}</p>
+    <p>{{ year }}</p>
+    <p>{{ month }}</p>
+    <p>{{ daysInMonth }}</p>
+    <p>{{ monthName }}</p>
+  </div>
 </template>
