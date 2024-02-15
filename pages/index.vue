c3b7f7def5096d06d4300e3e53db384e5e0f7141
Bruno Braga <brunobragaw8t@gmail.com>
2024-02-15 19:13:25 +0000

    feature: make prev and next buttons work for months

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,44 @@
 <script setup lang="ts">
 const route = useRoute()
 
-let year: number
-let month: number
+const year = ref(new Date().getFullYear())
+const month = ref(new Date().getMonth() + 1)
 
 if (
   typeof route.query.month === 'string' &&
   /^\d{4}-\d{2}$/.test(route.query.month)
 ) {
   const [yearStr, monthStr] = route.query.month.split('-')
-  year = parseInt(yearStr)
-  month = parseInt(monthStr)
-} else {
-  year = new Date().getFullYear()
-  month = new Date().getMonth() + 1
+  year.value = parseInt(yearStr)
+  month.value = parseInt(monthStr)
 }
 
-const daysInMonth = new Date(year, month, 0).getDate()
-const monthName = new Date(year, month - 1).toLocaleString('en-GB', { month: 'long' })
+const daysInMonth = computed(() => {
+  return new Date(year.value, month.value, 0).getDate()
+})
+
+const monthName = computed(() => {
+  return new Date(year.value, month.value - 1)
+    .toLocaleString('en-GB', { month: 'long' })
+})
+
+function previousMonth () {
+  if (month.value === 1) {
+    year.value--
+    month.value = 12
+  } else {
+    month.value--
+  }
+}
+
+function nextMonth () {
+  if (month.value === 12) {
+    year.value++
+    month.value = 1
+  } else {
+    month.value++
+  }
+}
 </script>
 
 <template>
@@ -26,8 +47,19 @@ const monthName = new Date(year, month - 1).toLocaleString('en-GB', { month: 'lo
       <h2>{{ monthName }} {{ year }}</h2>
 
       <div class="d-flex gap-1">
-        <AppButton label="Previous" size="sm" variant="secondary" />
-        <AppButton label="Next" size="sm" variant="secondary" />
+        <AppButton
+          label="Previous"
+          size="sm"
+          variant="secondary"
+          @click="previousMonth"
+        />
+
+        <AppButton
+          label="Next"
+          size="sm"
+          variant="secondary"
+          @click="nextMonth"
+        />
       </div>
     </div>
 
