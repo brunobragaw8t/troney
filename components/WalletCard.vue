e1f4d47d16cdf54e84041b8e1562699d245d412a
Bruno Braga <brunobragaw8t@gmail.com>
2024-02-17 14:50:48 +0000

    feature: create WalletCard component

---
+++ components/WalletCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { currencyFormat } from '~/helpers/currency-format'
+
+defineProps<{
+  name: string
+  balance: number
+}>()
+</script>
+
+<template>
+  <div class="border p-3 text-center">
+    <Icon name="fa6-solid:wallet" size="3rem" class="mb-2" />
+
+    <h6 class="mb-1">
+      {{ name }}
+    </h6>
+
+    <h4 class="mb-0">
+      {{ currencyFormat(balance) }}
+    </h4>
+  </div>
+</template>
