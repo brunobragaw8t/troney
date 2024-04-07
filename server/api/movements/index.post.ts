bc522c6d9efc525a35ce3c609c711793489ec0a6
Bruno Braga <brunobragaw8t@gmail.com>
2024-04-07 22:00:08 +0100

    feature: create API route to create movement

---
+++ server/api/movements/index.post.ts
@@ -0,0 +1,45 @@
+import { serverSupabaseClient } from '#supabase/server'
+import { z } from 'zod'
+import { Database, TablesInsert } from '~/types/supabase'
+
+const createMovementSchema = z.object({
+  description: z.string(),
+  wallet_id_from: z.union([z.number(), z.null()]),
+  wallet_id_to: z.union([z.number(), z.null()]),
+  value: z.number()
+})
+
+export default defineEventHandler(async (event) => {
+  const payload = await readValidatedBody(event, createMovementSchema.safeParse)
+
+  if (!payload.success) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: payload.error.message
+    })
+  }
+
+  const body = payload.data
+
+  const sbClient = await serverSupabaseClient<Database>(event)
+
+  const { data, error } = await sbClient.from('movements')
+    .insert<TablesInsert<'movements'>>({
+      description: body.description,
+      wallet_id_from: body.wallet_id_from,
+      wallet_id_to: body.wallet_id_to,
+      value: body.value
+    })
+    .select()
+
+  if (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: error.message
+    })
+  }
+
+  setResponseStatus(event, 201)
+
+  return data[0]
+})
