d76bdd103a3454de8740205b78beec27e2ce807f
Bruno Braga <brunobragaw8t@gmail.com>
2024-04-29 12:28:43 +0100

    feature: create sortByKey function along with its test

---
+++ helpers/sort-by-key.ts
@@ -0,0 +1,13 @@
+export function sortByKey<T> (arr: T[], key: keyof T): T[] {
+  return [...arr].sort((a, b) => {
+    if (a[key] < b[key]) {
+      return -1
+    }
+
+    if (a[key] > b[key]) {
+      return 1
+    }
+
+    return 0
+  })
+}
