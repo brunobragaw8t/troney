14b75f8c6c983d2f443f1247b94a58fc36438aae
Bruno Braga <brunobragaw8t@gmail.com>
2024-05-11 15:46:13 +0100

    chore: delete obsolete getRandomColor function

--- helpers/get-random-color.nuxt.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from 'vitest'
-import { getRandomColor } from './get-random-color'
-
-describe('getRandomColor', () => {
-  test('basic', () => {
-    expect(getRandomColor()).toMatch(/^#[0-9a-f]{6}$/)
-  })
-})
